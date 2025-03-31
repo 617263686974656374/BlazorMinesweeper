e47d34fa07794fab9be956e8fc87a019f77dcc8c
Vaše meno <jaroslav.krc@gmail.com>
2025-03-31 19:26:34 +0200

    Add project files.

---
+++ BlazorClassicMines/BlazorClassicMines.Client/Code/Point.cs
@@ -0,0 +1,51 @@
+﻿namespace BlazorClassicMines.Client.Code
+{
+    public class Point
+    {
+
+        public bool Uncovered { get; private set; }
+
+        public bool Marked { get; private set; }
+        public bool Clickable => !Marked && !Uncovered;
+
+        public bool Shine { get; private set; } = false;
+
+        public string CssClass =>
+            (Shine ? " svitit" : "") +
+            (Typ == 9 ? " mina" : "") +
+            (Typ > 0 ? $" cislo-{Typ}" : "");
+
+        public int X { get; init; }
+        public int Y { get; init; }
+
+        int typ = 0;
+        public int Typ { get => typ; set { if (value >= 0 && value <= 9) typ = value; } }
+
+        public event EventHandler Uncover;
+
+        public Point(int y, int x, EventHandler uncover)
+        {
+            (Y, X) = (y, x);
+            Uncover += uncover;
+        }
+
+        public void Discover(bool triggerEvent)
+        {
+            if (!Clickable) return;
+            Uncovered = true;
+
+            if (triggerEvent)
+            {
+                if (Typ == 9) Shine = true;
+                Uncover?.Invoke(this, EventArgs.Empty);
+            }
+        }
+
+        public void TagPoint()
+        {
+            if (Uncovered) return;
+            Marked = !Marked;
+        }
+
+    }
+}
